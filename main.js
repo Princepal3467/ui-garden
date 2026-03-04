13686da9ed8beabd5eded38ff31d7952277ee726
Princepal3467 <psingh60@academic.rrc.ca>
2026-03-04 12:23:04 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+/** @type { import('@storybook/react-webpack5').StorybookConfig } */
+module.exports = {
+  stories: ["../src/**/*.stories.@(ts|tsx)"],
+  addons: ["@storybook/addon-essentials"],
+  framework: "@storybook/react-webpack5",
+};
